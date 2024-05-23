b58e73d30bad45031228cb03c4f9ef2ec551d85e
Muhammad Fadhilah Hanan <88243095+Vareniel@users.noreply.github.com>
2024-05-24 05:44:24 +0700

    Update sw-register.js

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,19 +1,18 @@
+import { Workbox } from 'workbox-window';
+
 const swRegister = async () => {
-  // eslint-disable-next-line no-undef
-  if (!("serviceWorker" in navigator)) {
-    // eslint-disable-next-line no-undef
-    console.log("Service Worker not supported in the browser");
+  if (!('serviceWorker' in navigator)) {
+    console.log('Service Worker not supported in the browser');
     return;
   }
 
+  const wb = new Workbox('./sw.bundle.js');
+
   try {
-    // eslint-disable-next-line no-undef
-    await navigator.serviceWorker.register("./sw.bundle.js");
-    // eslint-disable-next-line no-undef
-    console.log("Service worker registered");
+    await wb.register();
+    console.log('Service worker registered');
   } catch (error) {
-    // eslint-disable-next-line no-undef
-    console.log("Failed to register service worker", error);
+    console.log('Failed to register service worker', error);
   }
 };
 
